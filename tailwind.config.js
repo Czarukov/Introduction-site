c8f699cfba709aca167096c939a3863e26ee3bc9
Czaruko <cezary.mildner98@gmail.com>
2023-10-25 18:18:51 +0200

    Applied universal style and navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      'dadarkblue': '#0d2b45',
+      'dablue': '#203c56',
+      'dadarkpurple': '#544e68',
+      'dapurple': '#8d697a',
+      'dabrown': '#d08159',
+      'daorange': '#ffaa5e',
+      'daskin': '#ffd4a3',
+      'dawhite': '#ffecd6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
   },
   plugins: [],
 }
