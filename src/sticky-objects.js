92613ed08e0194af354e9fa04d2da37c0b48cf03
Czaruko <cezary.mildner98@gmail.com>
2023-11-12 20:38:03 +0100

    Experimented with navigation bar

--- src/sticky-objects.js
+++ src/sticky-objects.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     const nameOfPage = document.querySelector('#name-page').innerHTML;
-    this.body.innerHTML = `<nav class="mx-auto flex bg-da-czaruk-black p-5 sticky top-0 left-0 w-full shadow-2xl text-da-czaruk-white justify-between items-center h-[100px]"><a href="index.html" class="text-4xl p-5 font-bold flex-none"><img src="/assets/Logo.png" class="w-[50px] h-[50px] inline" alt="" />czaruk</a><div class="text-5xl">${nameOfPage}</div><div class="text-xl text-right inline flex-none"><a href="blog.html" class="p-3"> Studies </a><a href="finished.html" class="p-3"> Finished </a><a href="contact.html" class="p-3"> Contact </a></div></nav>` + this.body.innerHTML;
+    this.body.innerHTML = `<nav class=" flex bg-da-czaruk-black p-5 sticky top-0 left-0 shadow-2xl text-da-czaruk-white justify-between items-center h-[100px]  mx-auto"><a href="index.html" class="text-4xl p-5 font-bold flex-none"><img src="/assets/Logo.png" class="w-[50px] h-[50px] inline" alt="" /></a><div class="text-5xl align-middle">${nameOfPage}</div><div class="text-xl text-right inline flex-none"><a href="blog.html" class="p-3"> Studies </a><a href="finished.html" class="p-3"> Finished </a><a href="contact.html" class="p-3"> Contact </a></div></nav>` + this.body.innerHTML;
 });
+
