f580d3dfbd646d673c22ccb655668e0da58d7d1f
Czaruko <cezary.mildner98@gmail.com>
2023-11-13 11:12:20 +0100

    Template start

--- src/sticky-objects.js
+++ src/sticky-objects.js
@@ -1,5 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     const nameOfPage = document.querySelector('#name-page').innerHTML;
-    this.body.innerHTML = `<nav class=" flex bg-da-czaruk-black p-5 sticky top-0 left-0 shadow-2xl text-da-czaruk-white justify-between items-center h-[100px]  mx-auto"><a href="index.html" class="text-4xl p-5 font-bold flex-none"><img src="/assets/Logo.png" class="w-[50px] h-[50px] inline" alt="" /></a><div class="text-5xl align-middle">${nameOfPage}</div><div class="text-xl text-right inline flex-none"><a href="blog.html" class="p-3"> Studies </a><a href="finished.html" class="p-3"> Finished </a><a href="contact.html" class="p-3"> Contact </a></div></nav>` + this.body.innerHTML;
+    this.body.innerHTML = `<nav
+    class="flex justify-center bg-da-czaruk-black p-5 sticky top-0 left-0 shadow-2xl h-[100px]"
+  >
+    <div
+      class="flex w-[1200px] m-auto text-da-czaruk-white justify-between items-center"
+    >
+      <a href="index.html" class="text-4xl p-5 font-bold flex-none">
+        <img src="/assets/Logo.png" class="w-[50px] h-[50px] inline" alt="" />
+        Czaruko
+      </a>
+      <div class="text-xl text-right inline flex-none">
+        <a href="blog.html" class="p-3"> Studies </a>
+        <a href="finished.html" class="p-3"> Finished </a>
+        <a href="contact.html" class="p-3"> Contact </a>
+      </div>
+    </div>
+  </nav>` + this.body.innerHTML;
 });
 
